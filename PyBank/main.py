7582e9853f698711ac0df86b2eb763541bab4880
Yedidya <yedidyaerque@gmail.com>
2018-07-31 14:29:38 -0500

    open csv reader

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,9 +3,9 @@ import os
 import csv 
 
 # Find path and open csv
-csv_filepath = os.path.join("DataSets_Python","budget_data.csv")
+csv_filepath = os.path.join("PythonDataSet", "budget_data.csv")
 
 with open(csv_filepath, newline="") as csv_file:
-    csv_data_object = csv.reader(csv_filepath, delimiter=",")
-    for row in csv_data_object:
+    csvreader = csv.reader(csv_file, delimiter=",")
+    for row in csvreader:
         print(row)
